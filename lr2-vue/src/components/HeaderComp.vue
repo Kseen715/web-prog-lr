68829c5e84e8e1da50e780cfa33e0400019803ed
kseen <korenevdn@gmail.com>
2024-10-18 20:56:34 +0300

    Refactor HeaderComp.vue for enhanced responsiveness and navigation adjustments

--- lr2-vue/src/components/HeaderComp.vue
+++ lr2-vue/src/components/HeaderComp.vue
@@ -15,7 +15,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <!-- <div class="horizontal-nav-container"> -->
         <nav class="horizontal-nav">
             <RouterLink to="/">Главная</RouterLink>
-            <RouterLink to="/shelf">Полка</RouterLink>
+            <RouterLink to="/" class="search-minimized">Поиск</RouterLink>
             <RouterLink to="/login">Логин</RouterLink>
         </nav>
         <!-- </div> -->
@@ -52,7 +52,7 @@ header h1 {
 
 header .logo-container {
     height: 80%;
-    padding: 10px;
+    /* padding: 10px; */
 }
 
 header .logo {
@@ -66,8 +66,13 @@ header .horizontal-nav {
     display: flex;
     gap: 20px;
     flex-direction: row;
+    /* justify-content: space-between; */
     justify-content: center;
+    /* justify-self: center; */
     align-items: center;
+    /* take full parent body */
+    height: 100%;
+
 }
 
 header .horizontal-nav a {
@@ -79,21 +84,75 @@ header .horizontal-nav a:hover {
     color: var(--color-accent-blue);
 }
 
-@media (max-width: 420px) {
+@media (max-width:961px) {
 
+    /* up to 961 */
+    .inventory-header {
+        display: none;
+    }
+
+    .search-minimized {
+        display: none;
+    }
+}
+
+@media (max-width:641px) {
+
+    /* up to 641 */
     .inventory-header,
-    .logo-container {
+    .search-bar {
+        display: none;
+    }
+
+    .search-minimized {
+        display: block;
+    }
+}
+
+@media (max-width:481px) {
+
+    /* up to 481 */
+    .inventory-header,
+    .logo-container,
+    .search-bar {
         display: none;
     }
 
     .header {
-        justify-content: center;
+        justify-self: center;
+    }
+
+    .search-minimized {
+        display: block;
+    }
+
+    .horizontal-nav {
+        width: 100%;
+    }
+
+    .search-minimized {
+        display: block;
     }
 }
 
-@media (max-width: 1600px) {
+@media (min-width:961px) {
+
+    /* more than 961 */
     .header {
-        justify-content: space-between;
+        justify-self: space-between;
+    }
+
+    .search-minimized {
+        display: none;
     }
 }
+
+/* 
+320px
+481px
+641px
+961px
+1025px
+1281px
+*/
 </style>

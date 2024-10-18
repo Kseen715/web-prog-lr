3c3fa827734a48faeb19853f04f5f87bc4e01d48
kseen <korenevdn@gmail.com>
2024-10-18 04:00:01 +0300

    Refactor HeaderComp.vue for improved layout and responsiveness

--- lr2-vue/src/components/HeaderComp.vue
+++ lr2-vue/src/components/HeaderComp.vue
@@ -7,16 +7,18 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink class="logo-container" to="/">
             <img class="logo" src="https://github.com/Kseen715/imgs/raw/main/sakura_kharune.png" alt="logo">
         </RouterLink>
-        <h1>Инвентарь</h1>
+        <h1 class="inventory-header">Инвентарь</h1>
         <div class="search-bar">
             <input type="text" placeholder="Поиск...">
             <button type="submit">Поиск</button>
         </div>
+        <!-- <div class="horizontal-nav-container"> -->
         <nav class="horizontal-nav">
             <RouterLink to="/">Главная</RouterLink>
             <RouterLink to="/shelf">Полка</RouterLink>
             <RouterLink to="/login">Логин</RouterLink>
         </nav>
+        <!-- </div> -->
     </header>
 </template>
 
@@ -27,12 +29,14 @@ header {
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
+    flex-wrap: wrap;
+
     padding: 0 20px;
     height: 15vh;
     color: var(--color-text-light);
     margin: 10px;
     border-radius: 16px;
+    justify-content: space-between;
 }
 
 header nav {
@@ -55,12 +59,15 @@ header .logo {
     height: 100%;
 }
 
-header .horizontal-nav ul {
+header .horizontal-nav {
     list-style-type: none;
     padding: 0;
     margin: 0;
     display: flex;
     gap: 20px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 }
 
 header .horizontal-nav a {
@@ -70,5 +77,23 @@ header .horizontal-nav a {
 
 header .horizontal-nav a:hover {
     color: var(--color-accent-blue);
+}
+
+@media (max-width: 420px) {
+
+    .inventory-header,
+    .logo-container {
+        display: none;
+    }
+
+    .header {
+        justify-content: center;
+    }
+}
+
+@media (max-width: 1600px) {
+    .header {
+        justify-content: space-between;
+    }
 }
 </style>
